3f48009b0c859a96d82f90b0e93c7ad6e9e4b38f
Charvonya Cox <64337036+VonyaC@users.noreply.github.com>
2021-06-23 10:34:36 -0400

    Added CORS

--- backend/main.py
+++ backend/main.py
@@ -3,9 +3,11 @@ from flask import Flask, request, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
+from flask_cors import CORS
 
 # Init app
 app = Flask(__name__)
+CORS(app)
 basedir = os.path.abspath(os.path.dirname(__file__))
 # Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
