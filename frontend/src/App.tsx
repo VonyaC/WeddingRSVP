4203e5fe2fcbed8d9d2cf9113bb3f664adf3986f
Stefano Hepburn <stefanohepburn@gmail.com>
2021-06-23 11:15:38 -0400

    wired in guest-list

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import './App.css';
-import  { Home, RSVP, Invited }  from './screens/screens';
-import { WeddingInfo } from './screens/WeddingInfo';
+import  { Home, RSVP, Invited, WeddingInfo, GuestList }  from './screens/screens';
 import { Navigation } from './components/Navigation';
 
 function App() {
@@ -14,6 +13,7 @@ function App() {
             <Route path='/wedding-info' component={WeddingInfo} />
             <Route path='/rsvp' component={RSVP}/>
             <Route path='/invited/:code'> <Invited /></Route>
+            <Route path='/guest-list'> <GuestList /></Route>
             <Route exact path='/' component={Home}/>
             <Redirect to='/not-found' /> 
             <Route path='/not-found' />
--- frontend/src/screens/screens.tsx
+++ frontend/src/screens/screens.tsx
@@ -2,6 +2,7 @@ import { Home } from './Home';
 import { RSVP } from './RSVP';
 import {Invited} from './Invited';
 import {WeddingInfo} from './WeddingInfo';
+import {GuestList} from './GuestList';
 
 
-export {Home, RSVP, Invited, WeddingInfo}
+export {Home, RSVP, Invited, WeddingInfo, GuestList}
