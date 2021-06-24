f62938863d950f1c62ba8a82e4aacfebc539c2b9
Charvonya Cox <64337036+VonyaC@users.noreply.github.com>
2021-06-23 23:54:42 -0400

    Revert "Update main.py"
    This reverts commit ed2d265f1170531d444e18f7b77905c28c9cd019.

--- backend/main.py
+++ backend/main.py
@@ -10,8 +10,6 @@ app = Flask(__name__)
 CORS(app)
 basedir = os.path.abspath(os.path.dirname(__file__))
 # Database
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
-#     os.path.join(basedir, 'db.sqlite')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://fheqbrylgdgtdz:4ff8d24aaca42d1882e92fecd8a518dba318983da6010f9360b8b9ca159508f2@ec2-34-202-54-225.compute-1.amazonaws.com:5432/dec943tmbs3gla'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Init db
